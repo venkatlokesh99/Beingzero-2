601d197dd755b17409f06fb110063b47df1a4956
Vejendla Venkat Lokesh <venkatlokesh999@gmail.com>
2019-12-26 11:06:49 +0530

    commit 2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express=require('express')
 const app=express()
-port= process.env.port||3000
+port= process.env.PORT||3000
 app.get('/',(req,res)=>res.send('Hello World!'))
 app.listen(port,()=>console.log(`Listening to port ${port}!`))
